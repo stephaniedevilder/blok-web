b1a05bfb0e34fa089fb413bfb165dee1e035839a
stephaniedevilder <94384514+stephaniedevilder@users.noreply.github.com>
2021-12-14 14:05:52 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 // JavaScript Document
+// OBSERVER
+
+var dropdownMenu = document.querySelector ("nav");
+
+function menuOpenDicht(){
+    document.documentElement.classList.remove("loading");
+	dropdownMenu.classList.toggle("transitie_navigatie");
+
+}
+
+dropdownMenu.addEventListener("click", menuOpenDicht);
+
